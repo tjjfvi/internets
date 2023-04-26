e34b8274f7183d07f59d5d3488790caeebe93ecc
tjjfvi <t6@t6.fyi>
2023-04-26 11:23:44 -0400

    rewrite

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,104 @@
-mod parser;
 mod program;
-mod runtime;
+use program::*;
 
-use std::{env, fs};
+#[derive(Clone, Copy, Debug)]
+struct Nat;
 
-use crate::{parser::parse, program::build_program, runtime::Runtime};
+impl Nat {
+  pub const ERASE: Kind = Kind(0);
+  pub const CLONE: Kind = Kind(1);
+  pub const ZERO: Kind = Kind(2);
+  pub const SUCC: Kind = Kind(3);
+  pub const ADD: Kind = Kind(4);
+}
+
+impl Net for Nat {
+  fn reduce(&self, mem: &mut Mem, pair: ActivePair) {
+    let ActivePair(a_kind, a_addr, b_kind, b_addr) = if pair.0 > pair.2 {
+      ActivePair(pair.2, pair.3, pair.0, pair.1)
+    } else {
+      pair
+    };
+    match (a_kind, b_kind) {
+      (Nat::ERASE, Nat::ZERO) => {}
+      (Nat::CLONE, Nat::ZERO) => {
+        mem.relink_nullary(a_addr + RelAddr::new(1), Nat::ZERO);
+        mem.relink_nullary(a_addr + RelAddr::new(2), Nat::ZERO);
+        mem.free(a_addr, 3);
+      }
+      (Nat::ERASE, Nat::SUCC) => {
+        mem.relink_nullary(b_addr + RelAddr::new(1), Nat::ERASE);
+        mem.free(b_addr, 2);
+      }
+      (Nat::CLONE, Nat::SUCC) => {
+        let chunk = mem.alloc(&[
+          Word::kind(Nat::SUCC),
+          Word::port(RelAddr::new(4), PortMode::Auxiliary),
+          Word::kind(Nat::SUCC),
+          Word::port(RelAddr::new(3), PortMode::Auxiliary),
+          Word::kind(Nat::CLONE),
+          Word::port(RelAddr::new(-4), PortMode::Auxiliary),
+          Word::port(RelAddr::new(-3), PortMode::Auxiliary),
+        ]);
+        mem.relink_principal(
+          b_addr + RelAddr::new(1),
+          Nat::CLONE,
+          chunk + RelAddr::new(4),
+        );
+        mem.relink_principal(a_addr + RelAddr::new(1), Nat::SUCC, chunk);
+        mem.relink_principal(a_addr + RelAddr::new(2), Nat::SUCC, chunk + RelAddr::new(2));
+        mem.free(a_addr, 3);
+        mem.free(b_addr, 2);
+      }
+      (Nat::ZERO, Nat::ADD) => {
+        mem.relink(b_addr + RelAddr::new(1), b_addr + RelAddr::new(2));
+        mem.free(b_addr, 3);
+      }
+      (Nat::SUCC, Nat::ADD) => {
+        let a_pred = a_addr + RelAddr::new(1);
+        let b_out = b_addr + RelAddr::new(2);
+        mem.relink_principal(b_out, Nat::SUCC, a_addr);
+        mem.relink_principal(a_pred, Nat::ADD, b_addr);
+        mem.link_auxillary(a_pred, b_out);
+      }
+      _ => unimplemented!(),
+    }
+  }
+}
 
 fn main() {
-  let args: Vec<_> = env::args().collect();
-  let program = build_program(&parse(&fs::read_to_string(&args[1]).unwrap()));
-  dbg!(&program);
-  let mut runtime = Runtime::new(&program);
-  dbg!(&runtime);
-  while !runtime.normal() {
-    // dbg!(&runtime);
-    runtime.reduce();
+  let mut mem = Mem::new(64);
+  let base = mem.alloc(&[
+    Word::port(RelAddr::new(3), PortMode::Auxiliary),
+    Word::kind(Nat::ADD),
+    Word::port(RelAddr::new(4), PortMode::Auxiliary),
+    Word::port(RelAddr::new(-3), PortMode::Auxiliary),
+    Word::kind(Nat::CLONE),
+    Word::port(RelAddr::new(-4), PortMode::Principal),
+    Word::port(RelAddr::new(-4), PortMode::Auxiliary),
+    Word::kind(Nat::ADD),
+    Word::port(RelAddr::new(4), PortMode::Auxiliary),
+    Word::port(RelAddr::new(-5), PortMode::Principal),
+    Word::kind(Nat::CLONE),
+    Word::port(RelAddr::new(-4), PortMode::Principal),
+    Word::port(RelAddr::new(-4), PortMode::Auxiliary),
+    Word::kind(Nat::SUCC),
+    Word::port(RelAddr::new(1), PortMode::Principal),
+    Word::kind(Nat::SUCC),
+    Word::kind(Nat::ZERO),
+  ]);
+  mem.active.push(ActivePair(
+    Nat::CLONE,
+    base + RelAddr::new(10),
+    Nat::SUCC,
+    base + RelAddr::new(13),
+  ));
+  dbg!(&mem);
+  let mut count = 0;
+  while let Some(pair) = mem.active.pop() {
+    count += 1;
+    Nat.reduce(&mut mem, pair);
+    dbg!(&mem);
   }
-  dbg!(&runtime);
-  dbg!(runtime.dbg_tree(runtime.get(runtime.free_port(0))));
+  dbg!(count);
 }
--- src/program.rs
+++ src/program.rs
@@ -1,216 +1,232 @@
 use std::{
-  collections::{hash_map::Entry, HashMap},
-  ops::Range,
+  fmt::Debug,
+  mem::size_of,
+  ops::{Add, Sub},
 };
 
-use itertools::Itertools;
+#[derive(Clone, Copy)]
+pub struct Word(u32);
 
-use crate::parser;
-
-#[derive(Debug)]
-pub struct Program {
-  pub types: Vec<Type>,
-  pub kinds: Vec<Kind>,
-  pub init: Init,
+impl Debug for Word {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    self.unpack().fmt(f)
+  }
 }
 
-#[derive(Debug)]
-pub struct Type {
-  pub id: usize,
-  pub name: String,
-  pub nodes: Range<usize>,
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum PortMode {
+  Auxiliary = 0,
+  Principal = 1,
 }
 
-#[derive(Debug)]
-pub struct Kind {
-  pub id: usize,
-  pub arity: usize,
-  pub name: String,
-  pub args: Vec<(bool, usize)>,
-  pub rules: HashMap<usize, Rule>,
+#[derive(Clone, Copy, Debug)]
+pub enum UnpackedWord {
+  Null,
+  Kind(Kind),
+  Port(RelAddr, PortMode),
 }
 
-#[derive(Debug)]
-pub struct Rule {
-  pub vars: usize,
-  pub left: Vec<Net>,
-  pub right: Vec<Net>,
-}
+pub const WORD_SIZE: usize = 4;
+const _WORD_SIZE_IS_FOUR: [u8; WORD_SIZE] = [0; size_of::<Word>()];
 
-#[derive(Debug)]
-pub enum Net {
-  Node(Node),
-  Var(Var),
-}
-
-#[derive(Debug, Clone, Copy)]
-pub struct Var {
-  pub id: usize,
-  pub def: bool,
-}
-
-#[derive(Debug)]
-pub struct Node {
-  pub kind: usize,
-  pub arity: usize,
-  pub ports: Vec<Net>,
-}
-
-#[derive(Debug)]
-pub struct Init {
-  pub vars: usize,
-  pub nets: Vec<(Node, Net)>,
-  pub free: Vec<usize>,
-}
-
-pub fn build_program(ast: &parser::Program) -> Program {
-  let mut types = Vec::new();
-  let mut nodes = Vec::new();
-  let mut type_ids = HashMap::new();
-  let mut node_ids = HashMap::new();
-  for type_ast in &ast.0 {
-    let id = types.len();
-    let nodes_start = nodes.len();
-    let nodes_end = nodes_start + type_ast.1.len();
-    types.push(Type {
-      id,
-      name: type_ast.0.to_owned(),
-      nodes: nodes_start..nodes_end,
-    });
-    type_ids.insert(type_ast.0, id);
-    for node_ast in &type_ast.1 {
-      let id = nodes.len();
-      nodes.push(Kind {
-        id,
-        arity: node_ast.1.len(),
-        name: node_ast.0.to_owned(),
-        args: Vec::with_capacity(node_ast.1.len()),
-        rules: HashMap::new(),
-      });
-      node_ids.insert(node_ast.0, id);
+impl Word {
+  #[inline(always)]
+  pub fn unpack(self) -> UnpackedWord {
+    match self.0 & 0b11 {
+      0 => UnpackedWord::Null,
+      1 => UnpackedWord::Kind(Kind(self.0 >> 2)),
+      _ => UnpackedWord::Port(
+        RelAddr((self.0 & !0b11) as i32),
+        match self.0 & 0b1 {
+          0 => PortMode::Auxiliary,
+          _ => PortMode::Principal,
+        },
+      ),
     }
   }
-  for type_ast in &ast.0 {
-    for node_ast in &type_ast.1 {
-      let id = node_ids[node_ast.0];
-      let node = &mut nodes[id as usize];
-      for &(sign, other) in &node_ast.1 {
-        node.args.push((sign, type_ids[other]));
-      }
-      for rule_ast in &node_ast.2 {
-        let parser::RuleNet::Node(left_name, left_children) = &rule_ast.0 else {
-          panic!("cannot use var directly in rule")
-        };
-        let parser::RuleNet::Node(right_name, right_children) = &rule_ast.1 else {
-          panic!("cannot use var directly in rule")
-        };
-        if right_name != &node_ast.0 {
-          panic!("expected right side of rule to be the parent")
-        }
-        dbg!(&left_name);
-        let other = node_ids[left_name];
-        let mut vars = HashMap::new();
-        let mut var_count = 0;
-        let mut cb = |ast| build_rule_node(ast, &nodes, &node_ids, &mut vars, &mut var_count);
-        let left = left_children.iter().map(&mut cb).collect_vec();
-        let right = right_children.iter().map(&mut cb).collect_vec();
-        for (name, var) in vars {
-          if !var.def {
-            panic!("var {} only used once", name)
-          }
-        }
-        let node = &mut nodes[id as usize];
-        node.rules.insert(
-          other,
-          Rule {
-            vars: var_count,
-            left,
-            right,
-          },
-        );
-      }
+  pub fn as_kind(self) -> Kind {
+    match self.unpack() {
+      UnpackedWord::Kind(kind) => kind,
+      _ => panic!("expected Kind word, got {:?}", self),
     }
   }
-  let mut init_nets = Vec::new();
-  let mut init_vars = HashMap::new();
-  let mut init_var_count = 0;
-  for (left, right) in &ast.1 .0 {
-    let mut cb = |ast| build_rule_node(ast, &nodes, &node_ids, &mut init_vars, &mut init_var_count);
-    let Net::Node(left) = cb(left) else {
-      panic!("expected left side to be a node")
-    };
-    let right = cb(right);
-    init_nets.push((left, right));
+  pub fn null() -> Self {
+    Word(0)
   }
-  Program {
-    types,
-    kinds: nodes,
-    init: Init {
-      vars: init_var_count,
-      nets: init_nets,
-      free: init_vars
-        .values()
-        .filter(|x| !x.def)
-        .map(|x| x.id)
-        .collect(),
-    },
+  pub fn kind(kind: Kind) -> Self {
+    Word((kind.0 as u32) << 2 | 1)
+  }
+  pub fn port(upper: RelAddr, mode: PortMode) -> Self {
+    Word((upper.0 as u32) | 2 | mode as u32)
   }
 }
 
-fn build_rule_node<'a>(
-  ast: &parser::RuleNet<'a>,
-  nodes: &Vec<Kind>,
-  node_ids: &HashMap<&'a str, usize>,
-  vars: &mut HashMap<&'a str, Var>,
-  var_count: &mut usize,
-) -> Net {
-  match ast {
-    parser::RuleNet::Var(name) => match vars.entry(name) {
-      Entry::Occupied(mut e) => {
-        let var = e.get_mut();
-        if var.def {
-          panic!("var {} used more than twice", name)
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub struct Kind(pub u32);
+
+#[derive(Clone, Copy, Debug)]
+pub struct Addr(*mut Word);
+
+impl Addr {
+  pub const NULL: Addr = Addr(0 as *mut Word);
+}
+
+#[derive(Clone, Copy, Debug)]
+pub struct RelAddr(i32);
+
+impl RelAddr {
+  pub fn new(idx: i32) -> RelAddr {
+    RelAddr(idx * 4)
+  }
+}
+
+impl Add<RelAddr> for Addr {
+  type Output = Addr;
+  fn add(self, rel: RelAddr) -> Self::Output {
+    Addr(((self.0 as isize) + (rel.0 as isize)) as *mut Word)
+  }
+}
+
+impl Sub<Addr> for Addr {
+  type Output = RelAddr;
+  fn sub(self, base: Addr) -> Self::Output {
+    RelAddr(((self.0 as isize) - (base.0 as isize)) as i32)
+  }
+}
+
+#[derive(Debug)]
+pub struct Mem {
+  buffer: Box<[Word]>,
+  alloc_idx: usize,
+  pub active: Vec<ActivePair>,
+}
+
+impl Mem {
+  pub fn new(size: usize) -> Self {
+    Mem {
+      buffer: vec![Word::null(); size].into_boxed_slice(),
+      alloc_idx: 0,
+      active: Vec::new(),
+    }
+  }
+  fn assert_valid(&self, addr: Addr, width: usize) {
+    let buffer_start = (&self.buffer[0]) as *const Word as usize;
+    let buffer_end = buffer_start + self.buffer.len() * WORD_SIZE;
+    assert!(addr.0 as usize >= buffer_start);
+    assert!(addr.0 as usize + width <= buffer_end);
+    assert!(addr.0 as usize & 0b11 == 0);
+  }
+  pub fn word(&self, addr: Addr) -> Word {
+    self.assert_valid(addr, WORD_SIZE);
+    unsafe { *addr.0 }
+  }
+  pub fn word_mut(&mut self, addr: Addr) -> &mut Word {
+    self.assert_valid(addr, WORD_SIZE);
+    unsafe { &mut *addr.0 }
+  }
+  // pub fn u32(&self, addr: Addr) -> u32 {
+  //   self.word(addr).0
+  // }
+  // pub fn u32_mut(&mut self, addr: Addr) -> &mut u32 {
+  //   self.assert_valid(addr, 4);
+  //   unsafe { &mut *(addr.0 as *mut u32) }
+  // }
+  // pub fn u64(&self, addr: Addr) -> u64 {
+  //   self.assert_valid(addr, 8);
+  //   unsafe { *(addr.0 as *const u64) }
+  // }
+  // pub fn u64_mut(&mut self, addr: Addr) -> &mut u64 {
+  //   self.assert_valid(addr, 8);
+  //   unsafe { &mut *(addr.0 as *mut u64) }
+  // }
+  pub fn alloc(&mut self, data: &[Word]) -> Addr {
+    let old_idx = self.alloc_idx;
+    self.alloc_idx += data.len();
+    self.buffer[old_idx..self.alloc_idx].copy_from_slice(data);
+    Addr(&mut self.buffer[old_idx] as *mut Word)
+  }
+  pub fn free(&mut self, addr: Addr, len: usize) {
+    self.assert_valid(addr, len * WORD_SIZE);
+    unsafe { std::slice::from_raw_parts_mut(addr.0, len) }.fill(Word::null());
+  }
+
+  pub fn relink(&mut self, a: Addr, b: Addr) {
+    match self.word(b).unpack() {
+      UnpackedWord::Kind(kind) => self.relink_nullary(a, kind),
+      UnpackedWord::Port(rel, PortMode::Auxiliary) => self.relink_auxillary(a, b + rel),
+      UnpackedWord::Port(rel, PortMode::Principal) => {
+        self.relink_principal(a, self.word(b + rel).as_kind(), b + rel)
+      }
+      _ => unreachable!(),
+    }
+  }
+
+  pub fn relink_nullary(&mut self, old: Addr, kind: Kind) {
+    let old_port = self.word(old);
+    match old_port.unpack() {
+      UnpackedWord::Kind(_) => {
+        // two nullary agents annihilate
+      }
+      UnpackedWord::Port(rel, other_mode) => {
+        let addr = old + rel;
+        if other_mode == PortMode::Auxiliary {
+          *self.word_mut(addr) = Word::kind(kind)
+        } else {
+          self.active.push(ActivePair(
+            self.word(addr).as_kind(),
+            addr,
+            kind,
+            Addr::NULL,
+          ))
         }
-        var.def = true;
-        Net::Var(*var)
       }
-      Entry::Vacant(e) => {
-        let var = Var {
-          id: *var_count,
-          def: false,
-        };
-        *var_count += 1;
-        e.insert(var);
-        Net::Var(var)
+      _ => unreachable!(),
+    }
+  }
+
+  pub fn link_auxillary(&mut self, a: Addr, b: Addr) {
+    *self.word_mut(a) = Word::port(b - a, PortMode::Auxiliary);
+    *self.word_mut(b) = Word::port(a - b, PortMode::Auxiliary);
+  }
+
+  pub fn relink_auxillary(&mut self, old: Addr, new: Addr) {
+    let old_port = self.word(old);
+    match old_port.unpack() {
+      UnpackedWord::Kind(_) => *self.word_mut(new) = old_port,
+      UnpackedWord::Port(rel, other_mode) => {
+        let addr = old + rel;
+        *self.word_mut(new) = Word::port(addr - new, other_mode);
+        if other_mode == PortMode::Auxiliary {
+          *self.word_mut(addr) = Word::port(new - addr, PortMode::Auxiliary);
+        }
       }
-    },
-    parser::RuleNet::Node(name, ports) => {
-      let kind = node_ids[name];
-      let arity = nodes[kind as usize].arity;
-      Net::Node(Node {
-        kind,
-        arity,
-        ports: ports
-          .iter()
-          .map(|ast| build_rule_node(ast, nodes, node_ids, vars, var_count))
-          .collect(),
-      })
+      _ => unreachable!(),
+    }
+  }
+
+  pub fn relink_principal(&mut self, old: Addr, new_kind: Kind, new: Addr) {
+    match self.word(old).unpack() {
+      UnpackedWord::Kind(kind) => self
+        .active
+        .push(ActivePair(kind, Addr::NULL, new_kind, new)),
+      UnpackedWord::Port(rel, other_mode) => {
+        let addr = old + rel;
+        if other_mode == PortMode::Auxiliary {
+          *self.word_mut(addr) = Word::port(new - addr, PortMode::Principal);
+        } else {
+          self
+            .active
+            .push(ActivePair(self.word(addr).as_kind(), addr, new_kind, new))
+        }
+      }
+      _ => unreachable!(),
     }
   }
 }
 
-impl Program {
-  pub fn max_arity(&self) -> usize {
-    self.kinds.iter().map(|x| x.arity).max().unwrap_or(0)
-  }
-  pub fn max_vars(&self) -> usize {
-    self
-      .kinds
-      .iter()
-      .flat_map(|x| x.rules.values())
-      .map(|x| x.vars)
-      .max()
-      .unwrap_or(0)
-      .max(self.init.vars)
-  }
+#[derive(Debug)]
+pub struct ActivePair(pub Kind, pub Addr, pub Kind, pub Addr);
+
+pub trait Net {
+  fn reduce(&self, mem: &mut Mem, pair: ActivePair);
 }
