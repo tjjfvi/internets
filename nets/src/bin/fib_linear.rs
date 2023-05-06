fcfd9c983abbfdf85ea696232ca1d7929c06c59e
vjjft <vjjft@vjjft.com>
2023-05-06 12:10:24 -0400

    update fib

--- nets/src/bin/fib_linear.rs
+++ nets/src/bin/fib_linear.rs
@@ -1,62 +1,46 @@
 use internets_nets::*;
 
+mod stdlib;
+
 interactions! {
   type FibLinear;
 
-  struct U64(+U64, $u64);
-
-  struct Add(-U64, -U64, +U64);
-  struct AddX(-U64, +U64, $u64);
-
-  struct Clone(-U64, +U64, +U64);
-  struct Erase(-U64);
-  struct Print(-U64);
-
-  impl Add(_, i, o) for U64(_, $n) { AddX(i, o, $n) }
-  impl AddX(_, o, $x) for U64(_, $y) { U64(o, $(x.wrapping_add(y))) }
-
-  impl Clone(_, o1, o2) for U64(_, $n) {
-    U64(o1, $n)
-    U64(o2, $n)
-  }
-  impl Erase(_) for U64(_, $_) {}
-  impl Print(_) for U64(_, $n) if { println!("{}", n); true } {}
-  impl Print(_) for U64(_, $_) {}
+  use stdlib::Std;
 
   struct Fib(-U64,+U64);
-  impl Fib(_, o) for U64(_, $0) { U64(o, $0) }
-  impl Fib(_, o) for U64(_, $n) {
-    U64(i,$n-1)
-    U64(f0,$0)
-    U64(f1,$1)
+  impl Fib(_, o) for Std::U64(_, $0) { Std::U64(o, $0) }
+  impl Fib(_, o) for Std::U64(_, $n) {
+    Std::U64(i,$n-1)
+    Std::U64(f0,$0)
+    Std::U64(f1,$1)
     FibP(i, f1, f0, o )
   }
 
   struct FibP(-U64,-U64,-U64,+U64);
-  impl FibP(_, o, a, o) for U64(_, $0) { Erase(a) }
-  impl FibP(_, b, a, o) for U64(_, $n) {
-    U64(i,$n-1)
+  impl FibP(_, o, a, o) for Std::U64(_, $0) { Std::Erase(a) }
+  impl FibP(_, b, a, o) for Std::U64(_, $n) {
+    Std::U64(i,$n-1)
     FibX( b, a, o, i)
   }
 
   struct FibX(-U64,-U64,+U64,-U64);
-  impl FibX(_, a, o, i) for U64(_, $b) {
-    U64(b,$b)
-    Clone(b,b1,b2)
-    Add(b1,a,c)
+  impl FibX(_, a, o, i) for Std::U64(_, $b) {
+    Std::U64(b,$b)
+    Std::Clone(b,b1,b2)
+    Std::Add(b1,a,c)
     FibP(i,c,b2,o)
   }
 
   fn main() {
-    U64(n, $1_000_000_000)
+    Std::U64(n, $1_000_000_000)
     Fib(n, o)
-    Print(o)
+    Std::Print(o)
   }
 }
 
 fn main() {
   let mut stats = Stats::default();
-  let mut net = Net::new(LinkAlloc::new(ArrayBuffer::new(1 << 20)));
+  let mut net = BasicNet::new(LinkAlloc::new(ArrayBuffer::new(1 << 20)));
   FibLinear::main(&mut net);
   reduce_with_stats(&mut net, &FibLinear, &mut stats);
   println!("{stats}");
--- nets/src/bin/fib_recursive.rs
+++ nets/src/bin/fib_recursive.rs
@@ -1,48 +1,32 @@
 use internets_nets::*;
 
+mod stdlib;
+
 interactions! {
   type FibRecursive;
 
-  struct U64(+U64, $u64);
-
-  struct Add(-U64, -U64, +U64);
-  struct AddX(-U64, +U64, $u64);
-
-  struct Clone(-U64, +U64, +U64);
-  struct Erase(-U64);
-  struct Print(-U64);
-
-  impl Add(_, i, o) for U64(_, $n) { AddX(i, o, $n) }
-  impl AddX(_, o, $x) for U64(_, $y) { U64(o, $(x.wrapping_add(y))) }
-
-  impl Clone(_, o1, o2) for U64(_, $n) {
-    U64(o1, $n)
-    U64(o2, $n)
-  }
-  impl Erase(_) for U64(_, $_) {}
-  impl Print(_) for U64(_, $n) if { println!("{}", n); true } {}
-  impl Print(_) for U64(_, $_) {}
+  use stdlib::Std;
 
   struct Fib(-U64,+U64);
-  impl Fib(_, o) for U64(_, $n @ (0|1)){ U64(o, $n) }
-  impl Fib(_, o) for U64(_, $n) {
-    Add(f1,f2,o)
+  impl Fib(_, o) for Std::U64(_, $n @ (0|1)){ Std::U64(o, $n) }
+  impl Fib(_, o) for Std::U64(_, $n) {
+    Std::Add(f1,f2,o)
     Fib(n1,f1)
     Fib(n2,f2)
-    U64(n1, ${n-1})
-    U64(n2, ${n-2})
+    Std::U64(n1, $n-1)
+    Std::U64(n2, $n-2)
   }
 
   fn main() {
-    U64(n, $40)
+    Std::U64(n, $40)
     Fib(n, o)
-    Print(o)
+    Std::Print(o)
   }
 }
 
 fn main() {
   let mut stats = Stats::default();
-  let mut net = Net::new(LinkAlloc::new(ArrayBuffer::new(1 << 20)));
+  let mut net = BasicNet::new(LinkAlloc::new(ArrayBuffer::new(1 << 20)));
   FibRecursive::main(&mut net);
   reduce_with_stats(&mut net, &FibRecursive, &mut stats);
   println!("{stats}");
